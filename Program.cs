50b1f127f7dfe3d0e362f74a7814abbb57f0e351
ELITEBOOK <ELITEBOOK@DESKTOP-7MJDDPC>
2022-11-22 03:11:01 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿
+public class Point
+{
+    private double Abscisse;            //L'attribut Abscisse
+    private double Ordonne;             //L'attribut Ordonne
+
+    public double getAbscisse()         //Le getter de l'attribut Abscisse
+    {
+        return Abscisse;
+    }
+
+    public void setAbscisse(double a)   //Le setter de l'attribut Abscisse
+    {
+        Abscisse = a;
+    }
+
+    public double getOrdonne()          //Le getter de l'attribut Ordonne
+    {
+        return Ordonne;
+    }
+
+    public void setOrdonne(double o)    //Le setter de l'attribut Ordonne
+    {
+        Ordonne = o;
+    }
+
+    public Point()                     //Le constructeur par défaut
+    {
+    }
+
+    public Point(double a, double o)   //Le constructeur d'initialisation
+    {
+        Abscisse = a;
+        Ordonne = o;
+    }
+
+    public double Norme()              //La méthode Norme
+    {
+        return Math.Sqrt(Abscisse * Abscisse + Ordonne * Ordonne);
+    }
+}
